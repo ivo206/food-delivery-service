2437aa237f91ccc74883a146534774f5fac3daa4
ivo206 <ivo23061987@gmail.com>
2022-02-26 21:06:12 +0000

    Create skeleton projects for the first basic microservices

---
+++ microservices/menu-item-composite-service/src/main/java/se/magnus/microservices/composite/menu/item/MenuItemCompositeServiceApplication.java
@@ -0,0 +1,13 @@
+package se.magnus.microservices.composite.menu.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuItemCompositeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuItemCompositeServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/menu-item-service/src/main/java/ivo/rakar/microservices/core/menu/item/MenuItemServiceApplication.java
@@ -0,0 +1,13 @@
+package ivo.rakar.microservices.core.menu.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuItemServiceApplication.class, args);
+	}
+
+}
